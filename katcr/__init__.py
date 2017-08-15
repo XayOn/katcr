a2d4fa87a532dd72aefb19e4c49fa730184bdf3c
David Francos Cuartero <dfrancos@buguroo.com>
2017-08-15 03:12:38 +0200

    Finished refactor. Now using bs4.

--- katcr/__init__.py
+++ katcr/__init__.py
@@ -1,51 +1,41 @@
 #!/usr/bin/env python3.5
 """Easy async python3.5 library to search magnets."""
 
-import re
-import urllib
 import asyncio
 import itertools
 from inquirer import List, prompt
 from docopt import docopt
 from blessings import Terminal
 from tabulate import tabulate
+from bs4 import BeautifulSoup
 import aiohttp
 
-TYPES = {'magnet': re.compile(r'\"magnet:\?xt=urn:(.+?)\"'),
-         'torrent': re.compile(r'\"//torcache.net(.+?)\"')}
 
-
-async def search_magnets(query, page, type_):
+async def search_magnets(query: str, page: int):
     """Return all links found in a single KAT page.
 
     This works by simple applying a regular expression to the page HTML
     (either for magnet or torrent).
     """
-    def parse_url(url):
-        """Parse url, either a magnet or a normal url.
-
-        Returns the query string and the torrent title
-        """
-        url = urllib.parse.urlparse(url)
-        query = urllib.parse.parse_qs(url.query)
-        query_ = query.get('dn', query.get('title', ''))[0]
-        if url.scheme == "magnet":
-            return "magnet:?xt={}".format(query['xt'][0]), query_
-        return "http://{}{}{}".format(*url[0:3]), query_
+    def make_printable(link):
+        """Make a given a href link parsed on bs4 printable."""
+        name = link.find(class_='cellMainLink').text
+        size = link.find_all('td')[1].text
+        return ('{} - {}'.format(name, size),
+                link.find('a', title='Torrent magnet link')['href'])
 
     with aiohttp.ClientSession() as session:
         url = 'https://thekat.info/usearch/{}/{}/'.format(query, page)
         async with session.get(url) as response:
-            assert response.status == 200
-            iter_ = TYPES[type_].finditer(
-                await response.text(), re.IGNORECASE)
-            return [parse_url(url.group(0)[1:-1]) for url in iter_]
+            torrents = BeautifulSoup(await response.text()).find_all(
+                id='torrent_latest_torrents')
+            return [make_printable(torrent) for torrent in torrents]
 
 
-def search(query, pagelen=1, type_="magnet"):
+def search(query: str, pagelen: int = 1):
     """Manage complete search for specified pages."""
     loop = asyncio.get_event_loop()
-    tasks = [asyncio.ensure_future(search_magnets(query, page + 1, type_))
+    tasks = [asyncio.ensure_future(search_magnets(query, page + 1))
              for page in range(0, pagelen)]
     loop.run_until_complete(asyncio.wait(tasks))
     return itertools.chain(*(a.result() for a in tasks))
@@ -59,14 +49,13 @@ def main():
     Options:
         --search=<SEARCH_TERM>   Search term(s)
         --pages=<PAGES_NUM>      Number of pages to lookup
-        --type=<magnet|torrent>  Preferred magnet type to look for
         -i --interactive         Enable interactive menu
         -h --help                Show this screen
     """
     opt = docopt(main.__doc__, version="0.0.1")
-    search_res = search(opt["--search"], int(opt["--pages"]), opt['--type'])
+    search_res = search(opt["--search"], int(opt["--pages"]))
     if not opt['--interactive']:
         return print(tabulate(search_res))
-    results = {b[:Terminal().width - 4]: a for a, b in search_res}
+    results = {a[:Terminal().width - 4]: b for a, b in search_res}
     print(results[prompt([List('Torrent', message="Choose",
                                choices=results.keys())])['Torrent']])
