30a1004fbd649f629be4a9799cfa16b012301d28
sarrablo <gsarrablo@gmail.com>
2017-08-16 13:10:20 +0200

    Fixed Style

--- katcr/bot.py
+++ katcr/bot.py
@@ -6,7 +6,8 @@ from docopt import docopt
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-import urllib
+
+
 class KATBot(telepot.Bot):
     """KAT.cr search bot, looks only for the first page."""
 
@@ -18,16 +19,20 @@ class KATBot(telepot.Bot):
         _, _, chat_id = telepot.glance(msg)
         self.sendMessage(chat_id, "Results for: {}".format(msg['text']))
         key = []
-        for a,b in search(msg['text'], 1):                       
-            key.append([InlineKeyboardButton(text=a, callback_data=a[:63])])
+        for key, _ in search(msg['text'], 1):
+            key.append([
+                InlineKeyboardButton(text=key, callback_data=key[:63])])
         keyboard = InlineKeyboardMarkup(inline_keyboard=key)
-        self.sendMessage(chat_id,  "Results for: {}".format(msg['text']), reply_markup=keyboard, parse_mode="html")
+        self.sendMessage(chat_id, "Results for: {}".format(msg['text']),
+                         reply_markup=keyboard, parse_mode="html")
 
-    def on_callback_query(self,msg):
-        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-        name = re.search(r"-(.*)-",query_data).group(1)
-        for a,b in search(name, 1):
-            self.sendMessage(from_id, b)
+    def on_callback_query(self, msg):
+        """Get the button data."""
+        _, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+        name = re.search(r"-(.*)-", query_data).group(1)
+        for _, value in search(name, 1):
+            self.sendMessage(from_id, value)
+
 
 def main():
     """Run telegram bot.
