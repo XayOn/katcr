39891a04ff0da21cf7642d59591c6143b7d4ea35
sarrablo <gsarrablo@gmail.com>
2017-08-16 14:36:14 +0200

    Fixed browsing

--- katcr/__init__.py
+++ katcr/__init__.py
@@ -11,32 +11,41 @@ import robobrowser
 import torrentmirror
 
 
-def search_magnets(query: str, page: int):
-    """Return all links found in a single KAT page.
+class Katcr:
+    """Katcr main class."""
 
-    This works by simple applying a regular expression to the page HTML
-    (either for magnet or torrent).
-    """
-    def make_printable(link):
-        """Make a given a href link parsed on bs4 printable."""
-        name = link.find(class_='cellMainLink').text
-        size = link.find_all('td')[1].text
-        return ('{} - {}'.format(name, size),
-                link.find('a', title='Torrent magnet link')['href'])
+    def __init__(self):
+        """Katcr constructor."""
+        self.browser = robobrowser.RoboBrowser(parser='html.parser')
 
-    # pylint: disable=not-callable
-    base_url = torrentmirror.get_proxies()['Kickass Torrents'][0][0]
-    http = '' if 'http' in base_url else 'http://'
-    page = robobrowser.RoboBrowser()
-    page.open('{}{}/search.php?q={}&p={}'.format(http, base_url, query, page))
-    torrents = page.find_all(id=re.compile('torrent_latest_torrents(.*)'))
-    return [make_printable(torrent) for torrent in torrents]
+    def search_magnets(self, query: str, page: int):
+        """Return all links found in a single KAT page.
 
+        This works by simple applying a regular expression to the page HTML
+        (either for magnet or torrent).
+        """
+        def make_printable(link):
+            """Make a given a href link parsed on bs4 printable."""
+            name = link.find(class_='cellMainLink').text
+            size = link.find_all('td')[1].text
+            return ('{} - {}'.format(name, size),
+                    link.find('a', title='Torrent magnet link')['href'])
 
-def search(query: str, pagelen: int = 1):
-    """Manage complete search for specified pages."""
-    return itertools.chain(*(search_magnets(query, page + 1)
-                             for page in range(0, pagelen)))
+        # pylint: disable=not-callable
+        base_url = torrentmirror.get_proxies()['Kickass Torrents'][0][0]
+        http = '' if 'http' in base_url else 'http://'
+        self.browser.open('{}{}/search.php?q={}&p={}'.format(http,
+                                                             base_url,
+                                                             query,
+                                                             page))
+        torrents = self.browser.find_all(id=re.compile(
+            'torrent_latest_torrents(.*)'))
+        return [make_printable(torrent) for torrent in torrents]
+
+    def search(self, query: str, pagelen: int = 1):
+        """Manage complete search for specified pages."""
+        return itertools.chain(*(self.search_magnets(query, page + 1)
+                                 for page in range(0, pagelen)))
 
 
 def main():
@@ -50,8 +59,9 @@ def main():
         -i --interactive         Enable interactive menu
         -h --help                Show this screen
     """
+    katcr = Katcr()
     opt = docopt(main.__doc__, version="0.0.1")
-    search_res = search(opt["--search"], int(opt["--pages"]))
+    search_res = katcr.search(opt["--search"], int(opt["--pages"]))
     if not opt['--interactive']:
         return print(tabulate(search_res))
     results = {a[:Terminal().width - 4]: b for a, b in search_res}
--- katcr/bot.py
+++ katcr/bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.5
 """Telegram bot to query KickassTorrents."""
 import re
-from katcr import search
+from katcr import Katcr
 from docopt import docopt
 import telepot
 from telepot.loop import MessageLoop
@@ -11,6 +11,11 @@ from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 class KATBot(telepot.Bot):
     """KAT.cr search bot, looks only for the first page."""
 
+    def __init__(self, token):
+        """Initialize of KATBot."""
+        super().__init__(token)
+        self.katcr = Katcr()
+
     # pylint: disable=too-few-public-methods
     def on_chat_message(self, msg):
         """Answer only chat messages."""
@@ -19,7 +24,7 @@ class KATBot(telepot.Bot):
         _, _, chat_id = telepot.glance(msg)
         self.sendMessage(chat_id, "Results for: {}".format(msg['text']))
         key = []
-        for key, _ in search(msg['text'], 1):
+        for key, _ in self.katcr.search(msg['text'], 1):
             key.append([
                 InlineKeyboardButton(text=key, callback_data=key[:63])])
         keyboard = InlineKeyboardMarkup(inline_keyboard=key)
@@ -30,7 +35,7 @@ class KATBot(telepot.Bot):
         """Get the button data."""
         _, from_id, query_data = telepot.glance(msg, flavor='callback_query')
         name = re.search(r"-(.*)-", query_data).group(1)
-        for _, value in search(name, 1):
+        for _, value in self.katcr.search(name, 1):
             self.sendMessage(from_id, value)
 
 
