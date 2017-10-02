09bd78f131e9983a959ea8a62f12cad7eaa2b973
David Francos Cuartero <dfrancos@buguroo.com>
2017-10-02 20:23:30 +0200

    Added magnet.sh, no tests yet.

--- katcr/__init__.py
+++ katcr/__init__.py
@@ -207,6 +207,25 @@ class Katcr(BaseSearch):
         return [self.tabulate(torrent) for torrent in torrents]
 
 
+class MagnetSh(BaseSearch):
+    """Magnet.sh torrent search API."""
+
+    proxy_name = ''
+    url_format = '{}{}/api/?q={}&s={}'
+    url = 'https://magnet.sh'
+
+    def get_torrents(self):
+        """Return a list of torrents."""
+        magnet = ('magnet:?xt=urn:btih:{}&tr=udp%3A%2F%2Ftracker.leechers-'
+                  'paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr='
+                  'udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftrack'
+                  'er.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.co'
+                  'm%3A6969')
+        return [(a['torrent_name'], 'Unknown',
+                 magnet.format(a['torrent_info_hash']))
+                for a in self.browser.response.json().values()]
+
+
 def get_short(where, what):
     """Get magnet short link."""
     return '{}/{}'.format(
@@ -257,11 +276,12 @@ def main():
 
     Currently available search engines:
 
-    - Katcr
+    - KickassTorrents
     - ThePirateBay
     - Nyaa
     - Skytorrents
     - Digbt
+    - Magnet.sh (Uses kickasstorrents)
 
     Options:
         -e --search-engines=<SearchEngine>  Torrent search engine to use
