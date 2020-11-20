781f2270e2cff9a6c0d6ff4ae99e7b03a33f2ee5
David Francos <opensource@davidfrancos.net>
2020-11-20 23:29:48 +0100

    Refs: #132 Added basic tests on base engine and cli

--- tests/unit/test_base_engine.py
+++ tests/unit/test_base_engine.py
@@ -29,6 +29,7 @@ async def test_search_magnets_without_proxies_with_results(torrent):
 
     result = await FakeSearch().search_magnets('foo', 1)
     assert result == [{'foo': 'bar'}]
+    torrent.assert_called()
 
 
 @pytest.mark.asyncio
@@ -48,3 +49,61 @@ async def test_search_magnets_without_proxies_without_results(torrent):
 
     result = await FakeSearch().search_magnets('foo', 1)
     assert result == []
+
+
+@pytest.mark.asyncio
+@patch('torrentmirror.get_proxies', side_effect=Exception)
+async def test_search_magnets_without_proxies_with_exceptions(torrent):
+    """Test search magnets method"""
+    from katcr.engines.base import BaseSearch
+
+    class FakeSearch(BaseSearch):
+        url = "http://foo.com"
+        url_format = ''
+        proxy_name = 'fake'
+        logger = logging.getLogger()
+
+        async def search_site(self, url):
+            return [{'foo': 'bar'}]
+
+    result = await FakeSearch().search_magnets('foo', 1)
+    assert result == [{'foo': 'bar'}]
+
+
+@pytest.mark.asyncio
+async def test_get_torrents():
+    from katcr.engines.base import BaseSearch
+
+    text = ('<a href="magnet:?xt=urn:btih:22222&dn=123">asdf</a>'
+            '<a href="magnet:?xt=urn:btih:1111111&dn=345">asdf</a>')
+
+    class FakeReq:
+        async def text(self):
+            return text
+
+    result = [a async for a in BaseSearch.get_torrents(FakeReq())]
+    assert result == [('123', 'magnet:?xt=urn:btih:22222&dn=123'),
+                      ('345', 'magnet:?xt=urn:btih:1111111&dn=345')]
+
+
+@pytest.mark.asyncio
+async def test_search():
+    from katcr.engines.base import BaseSearch
+    from unittest.mock import MagicMock, call
+    mock = MagicMock()
+
+    class FakeSearch(BaseSearch):
+        url = "http://foo.com"
+        url_format = ''
+        proxy_name = 'fake'
+        logger = logging.getLogger()
+
+        async def search_site(self, url):
+            return [{'foo': 'bar'}]
+
+        async def search_magnets(self, query, page):
+            mock(query, page)
+            return [1, 2]
+
+    assert await FakeSearch().search('a', 2) == [1, 2, 1, 2]
+    mock.assert_has_calls([call('a', 1), call('a', 2)])
---
+++ tests/unit/test_cli.py
@@ -0,0 +1,48 @@
+from unittest.mock import patch
+
+
+def test_result_noninteractive():
+    from katcr import Result
+    res = Result(['a' * 20, 'b' * 20], False)
+    assert str(res) == 'aaaaaaaaaaaaaaaaaaaa | bbbbbbbbbbbbbbbbbbbb'
+
+
+@patch('katcr.MAX_SIZE', 5)
+def test_result_ninteractive():
+    from katcr import Result
+    res = Result(['a' * 20, 'b' * 20], True)
+    assert str(res) == 'aaaaa'
+
+
+@patch('subprocess.check_call')
+def test_result_open(spmock):
+    from katcr import Result
+    res = Result(['a' * 20, 'b' * 20], True)
+    res.open()
+    spmock.assert_called_with(['xdg-open', 'b' * 20])
+
+
+class FakeEngines:
+    class fake:
+        async def search(*args, **kwargs):
+            return []
+
+
+def test_search_cmd():
+    from cleo import CommandTester
+    from katcr import CLICommand
+    from cleo import Application
+    from katcr.engines.base import BaseSearch
+
+    import katcr
+    katcr.engines = FakeEngines
+
+    assert CLICommand().logger is None
+    application = Application()
+    application.add(CLICommand())
+
+    command_tester = CommandTester(application.find('search'))
+    command_tester.execute('search --engines fake asdf')
+    assert BaseSearch.logger
+    assert BaseSearch.config
+    assert BaseSearch.session
